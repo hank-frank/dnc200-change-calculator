274483e5261c1cc5985fc9e2a6eae81d59554ed2
hankfrank <henryfrank1@yahoo.com>
2020-03-27 13:34:56 -0700

    tests passing

--- src/dnc200-change-calculator/Program.cs
+++ src/dnc200-change-calculator/Program.cs
@@ -14,19 +14,9 @@ namespace dnc200_change_calculator
       Console.WriteLine($"And How much hot dirty cash are you handing over?");
       decimal cash = Convert.ToDecimal(Console.ReadLine());
 
-      if (cost == cash)
-      {
-        Console.WriteLine("No Change Due");
-      }
-
-
       string results = Program.GetChange(cost, cash);
       Console.WriteLine(results);
 
-      //foreach (string message in results)
-      //{
-      //  Console.WriteLine(message);
-      //}
     }
 
 
@@ -40,11 +30,23 @@ namespace dnc200_change_calculator
       {
         string message = "You are still owed";
         finalStrings.Add(message);
+        changeDue = Math.Abs(changeDue);
+
+        decimal[] amounts = { 1m, 0.25m, 0.10m, 0.05m, 0.01m };
+
+        for (int i = 0; i < amounts.Length; i++)
+        {
+          decimal amount = Math.Floor(changeDue / amounts[i]);
+          decimal remainder = (changeDue % amounts[i]);
+          final.Add(amount);
+          changeDue = remainder;
+        }
+
       }
 
       if (changeDue == 0)
       {
-        string message = "No change due";
+        string message = "No change is due";
         finalStrings.Add(message);
       }
 
@@ -67,19 +69,26 @@ namespace dnc200_change_calculator
 
       if (results.Length > 1)
       {
-        string[] units = { "dollars", "quarters", "dimes", "nickles", "pennies" };
+        string[] units = { "dollars", "quarters", "dimes", "nickels", "pennies" };
+        string[] unitsSingular = { "dollar", "quarter", "dime", "nickel", "penny" };
         string preface = "The total change due is, \r\n";
         finalStrings.Add(preface);
         for (int i = 0; i < results.Length; i++)
-        {
-          string change = $"{results[i]} {units[i]},\r\n";
-          finalStrings.Add(change);
-        }
+          if (results[i] == 1)
+          {
+            string change = $"{results[i]} {unitsSingular[i]},\r\n";
+            finalStrings.Add(change);
+          } else
+          {
+            string change = $"{results[i]} {units[i]},\r\n";
+            finalStrings.Add(change);
+          }
+        
       }
 
       string[] resultArray = finalStrings.ToArray();
       string result = string.Join(" ", resultArray);
-      Console.WriteLine($"result {result}");
+
       return result;
     }
 
